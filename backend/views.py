021eeb844f2f56b76c403948efb5d39d2e746c18
Himanshujha2106 <himanshu.jha@ssipmt.com>
2024-03-21 14:33:27 +0530

    first

--- backend/views.py
+++ backend/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from .forms import UserCredentialsForm
 from django.http import HttpResponse
+from django.core.management.base import BaseCommand
+from backend.models import UserCredentials
 
 def user_input_view(request):
     if request.method == 'POST':
@@ -10,4 +12,7 @@ def user_input_view(request):
             return render(request,'success.html')  # Redirect to a success page or another URL
     else:
         form = UserCredentialsForm()
-    return render(request, 'user_input.html', {'form': form})
+
+
+    ans=UserCredentials.objects.get(username='himanshujha')
+    return render(request, 'user_input.html', {'form': form,'name':ans.username})
--- notes/urls.py
+++ notes/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.urls import path,include
-from backend.views import user_input_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
