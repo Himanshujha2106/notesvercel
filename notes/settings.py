9fd5dfebc77136df2157ed60dfd23c311b7e6528
Himanshujha2106 <himanshu.jha@ssipmt.com>
2024-03-21 15:33:47 +0530

    Deploying

--- notes/settings.py
+++ notes/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-atqp!y4_1i(=8246@90pr8sdt74uh#aw3*bhg*iin7ipnqjh98'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app','.now.sh','localhost','127.0.0.1','notes.himanshujha.xyz']
 
